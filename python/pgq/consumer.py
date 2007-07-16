6f0437143896a64b691955f654ec0b888033d791
Marko Kreen <markokr@gmail.com>
2007-07-16 14:06:21 +0000

    pgq.consumer: dont allow big seeking around

--- python/pgq/consumer.py
+++ python/pgq/consumer.py
@@ -334,6 +334,8 @@ class SerialConsumer(Consumer):
             return False
 
         if prev_tick < dst_tick:
+            if dst_tick - prev_tick > 5:
+                 raise Exception('Difference too big, skipping dangerous')
             self.log.warning('Got tick %d, dst has %d - skipping' % (prev_tick, dst_tick))
             return True
         else:
