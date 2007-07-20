e1c1f65d6a8c1f40134cc9af823cf29941ab2e0e
Marko Kreen <markokr@gmail.com>
2007-07-20 13:10:28 +0000

    pgq_ext: functions for tick tracking

--- sql/pgq_ext/sql/test_pgq_ext.sql
+++ sql/pgq_ext/sql/test_pgq_ext.sql
@@ -23,4 +23,14 @@ select pgq_ext.set_event_done('c', 3, 101);
 select pgq_ext.set_batch_done('c', 3);
 select * from pgq_ext.completed_event order by 1,2;
 
+--
+-- test tick tracking
+--
+select pgq_ext.get_last_tick('c');
+select pgq_ext.set_last_tick('c', 1);
+select pgq_ext.get_last_tick('c');
+select pgq_ext.set_last_tick('c', 2);
+select pgq_ext.get_last_tick('c');
+select pgq_ext.set_last_tick('c', NULL);
+select pgq_ext.get_last_tick('c');
 
