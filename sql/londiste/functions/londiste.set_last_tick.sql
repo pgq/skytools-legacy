9e1b196a710896a56ed073af462d01cdc09ddc2a
Marko Kreen <markokr@gmail.com>
2007-07-20 13:17:31 +0000

    londiste: make set_last_tick able to forget the position

--- sql/londiste/functions/londiste.set_last_tick.sql
+++ sql/londiste/functions/londiste.set_last_tick.sql
@@ -4,12 +4,17 @@ create or replace function londiste.set_last_tick(
     i_tick_id bigint)
 returns integer as $$
 begin
-    update londiste.completed
-       set last_tick_id = i_tick_id
-     where consumer_id = i_consumer;
-    if not found then
-        insert into londiste.completed (consumer_id, last_tick_id)
-            values (i_consumer, i_tick_id);
+    if i_tick_id is null then
+        delete from londiste.completed
+         where consumer_id = i_consumer;
+    else
+        update londiste.completed
+           set last_tick_id = i_tick_id
+         where consumer_id = i_consumer;
+        if not found then
+            insert into londiste.completed (consumer_id, last_tick_id)
+                values (i_consumer, i_tick_id);
+        end if;
     end if;
 
     return 1;
