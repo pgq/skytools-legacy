1695a8e21288d29ab27e9bf20947a0e2e654cdef
Marko Kreen <markokr@gmail.com>
2007-07-31 19:18:33 +0000

    londiste: remove unused denytrigger

--- sql/londiste/functions/londiste.provider_create_trigger.sql
+++ sql/londiste/functions/londiste.provider_create_trigger.sql
@@ -6,7 +6,6 @@ create or replace function londiste.provider_create_trigger(
 ) returns integer strict as $$
 declare
     tgname text;
-    sql    text;
 begin
     select trigger_name into tgname
         from londiste.provider_table
@@ -16,16 +15,13 @@ begin
         raise exception 'table not found';
     end if;
 
-    sql := 'select pgq.insert_event('
-        || quote_literal(i_queue_name)
-        || ', $1, $2, '
-        || quote_literal(i_table_name)
-        || ', NULL, NULL, NULL)';
     execute 'create trigger ' || tgname
         || ' after insert or update or delete on '
         || i_table_name
-        || ' for each row execute procedure logtriga($arg1$'
-        || i_col_types || '$arg1$, $arg2$' || sql || '$arg2$)';
+        || ' for each row execute procedure pgq.logtriga('
+        || quote_literal(i_queue_name) || ', '
+        || quote_literal(i_col_types) || ', '
+        || quote_literal(i_table_name) || ')';
 
     return 1;
 end;
