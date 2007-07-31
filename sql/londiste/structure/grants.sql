a946c4a512415fd82656b4e72087ae237380f104
Marko Kreen <markokr@gmail.com>
2007-07-31 09:59:20 +0000

    allow everybody to SELECT pgq and londiste tables

---
+++ sql/londiste/structure/grants.sql
@@ -0,0 +1,7 @@
+
+grant usage on schema londiste to public;
+grant select on londiste.provider_table to public;
+grant select on londiste.completed to public;
+grant select on londiste.link to public;
+grant select on londiste.subscriber_table to public;
+
---
+++ sql/pgq/structure/grants.sql
@@ -0,0 +1,10 @@
+
+grant usage on schema pgq to public;
+grant select on table pgq.consumer to public;
+grant select on table pgq.queue to public;
+grant select on table pgq.tick to public;
+grant select on table pgq.queue to public;
+grant select on table pgq.subscription to public;
+grant select on table pgq.event_template to public;
+grant select on table pgq.retry_queue to public;
+grant select on table pgq.failed_queue to public;
