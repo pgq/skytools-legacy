7c83d172a57b7bfff210bc62f0ce0ebd63676b8e
Marko Kreen <markokr@gmail.com>
2007-12-05 15:56:13 +0000

    make gendb work

--- tests/setadm/gendb.sh
+++ tests/setadm/gendb.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+. ../env.sh
+
 dropdb zset_root
 dropdb zset_branch
 dropdb zset_leaf
@@ -7,8 +9,8 @@ createdb zset_root
 createdb zset_branch
 createdb zset_leaf
 
-./admset.py master.ini init-root z-root "dbname=zset_root"
-./admset.py master.ini init-branch z-branch "dbname=zset_branch" --provider=z-root
-./admset.py master.ini init-leaf z-leaf "dbname=zset_leaf" --provider=z-branch
+setadm.py master.ini init-root z-root "dbname=zset_root"
+setadm.py master.ini init-branch z-branch "dbname=zset_branch" --provider=z-root
+setadm.py master.ini init-leaf z-leaf "dbname=zset_leaf" --provider=z-branch
 
 
