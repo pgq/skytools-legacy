9a1ccb19793cb24528a4a987d252e9a2e4679d93
Marko Kreen <markokr@gmail.com>
2008-02-28 17:40:33 +0000

    pgq/triggers/common.h: cleanup and comment

--- sql/pgq/triggers/common.h
+++ sql/pgq/triggers/common.h
@@ -1,13 +1,18 @@
+
+/*
+ * Per-event temporary data.
+ */
 struct PgqTriggerEvent {
 	const char *table_name;
 	const char *queue_name;
 	const char *ignore_list;
 	const char *pkey_list;
-	char op_type;
-	bool skip;
 
 	const char *attkind;
 	int attkind_len;
+
+	char op_type;
+	bool skip;
 
 	struct PgqTableInfo *info;
 
@@ -18,17 +23,12 @@ struct PgqTriggerEvent {
 };
 typedef struct PgqTriggerEvent PgqTriggerEvent;
 
-void pgq_prepare_event(struct PgqTriggerEvent *ev, TriggerData *tg, bool newstyle);
-
-
-char *pgq_find_table_name(Relation rel);
-void pgq_simple_insert(const char *queue_name, Datum ev_type, Datum ev_data, Datum ev_extra1);
-
-struct PgqColumnInfo {
-	int col_no;
-	char *col_name;
-};
-
+/*
+ * Per-table cached info.
+ *
+ * Can be shared between triggers on same table,
+ * so nothing trigger-specific should be stored.
+ */
 struct PgqTableInfo {
 	Oid oid;
 	char *table_name;
@@ -37,15 +37,14 @@ struct PgqTableInfo {
 	int *pkey_attno;
 };
 
-struct PgqTableInfo *
-pgq_find_table_info(Relation rel);
-
-
-int pgqtriga_make_sql(PgqTriggerEvent *ev, TriggerData *tg, StringInfo sql);
-
+/* common.c */
+struct PgqTableInfo *pgq_find_table_info(Relation rel);
+void pgq_prepare_event(struct PgqTriggerEvent *ev, TriggerData *tg, bool newstyle);
+char *pgq_find_table_name(Relation rel);
+void pgq_simple_insert(const char *queue_name, Datum ev_type, Datum ev_data, Datum ev_extra1);
 bool pgqtriga_skip_col(PgqTriggerEvent *ev, TriggerData *tg, int i, int attkind_idx);
 bool pgqtriga_is_pkey(PgqTriggerEvent *ev, TriggerData *tg, int i, int attkind_idx);
 
-
-
+/* makesql */
+int pgqtriga_make_sql(PgqTriggerEvent *ev, TriggerData *tg, StringInfo sql);
 
