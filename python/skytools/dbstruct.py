c077ec6cee6b5a3ba6296470dfe32bb2197eb3fb
Marko Kreen <markokr@gmail.com>
2009-10-23 12:01:55 +0000

    Remove ONLY from ADD CONSTRAINT.
    Parent table cannot have contraints that the childs do not have.

--- python/skytools/dbstruct.py
+++ python/skytools/dbstruct.py
@@ -89,7 +89,9 @@ class TConstraint(TElem):
             self.type += T_PKEY
 
     def get_create_sql(self, curs, new_table_name=None):
-        fmt = "ALTER TABLE ONLY %s ADD CONSTRAINT %s %s;"
+        # no ONLY here as table with childs (only case that matters)
+        # cannot have contraints that childs do not have
+        fmt = "ALTER TABLE %s ADD CONSTRAINT %s %s;"
         if new_table_name:
             name = self.name
             if self.contype in ('p', 'u'):
