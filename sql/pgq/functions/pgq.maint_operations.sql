4004826771dcc019317fb4043880fee6da8b1932
Marko Kreen <markokr@gmail.com>
2010-09-01 17:29:24 +0300

    new function too

---
+++ sql/pgq/functions/pgq.maint_operations.sql
@@ -0,0 +1,68 @@
+create or replace function pgq.maint_operations(out func_name text, out func_arg text)
+returns setof record as $$
+-- ----------------------------------------------------------------------
+-- Function: pgq.maint_operations(0)
+--
+--      Returns list of functions to call for maintenance.
+--
+--      The goal is to avoid hardcoding them into maintenance process.
+--
+-- Function signature:
+--      Function should take either 1 or 0 arguments and return 1 if it wants
+--      to be called immediately again, 0 if not.
+--
+-- Returns:
+--      func_name   - Function to call
+--      func_arg    - Optional argument to function (queue name)
+-- ----------------------------------------------------------------------
+declare
+    ops text[];
+begin
+    -- rotate step 1
+    func_name := 'pgq.maint_rotate_tables_step1';
+    for func_arg in
+        select queue_name from pgq.queue
+            where queue_rotation_period is not null
+                and queue_switch_step2 is not null
+                and queue_switch_time + queue_rotation_period < current_timestamp
+            order by 1
+    loop
+        return next;
+    end loop;
+
+    -- rotate step 2
+    func_name := 'pgq.maint_rotate_tables_step2';
+    func_arg := NULL;
+    return next;
+
+    -- check if extra field exists
+    perform 1 from pg_attribute
+      where attrelid = 'pgq.queue'::regclass
+        and attname = 'queue_extra_maint';
+    if found then
+        -- add extra ops
+        for func_arg, ops in
+            select q.queue_name, queue_extra_maint from pgq.queue q
+             where queue_extra_maint is not null
+             order by 1
+        loop
+            for i in array_lower(ops, 1) .. array_upper(ops, 1)
+            loop
+                func_name = ops[i];
+                return next;
+            end loop;
+        end loop;
+    end if;
+
+    -- vacuum tables
+    func_name := 'vacuum';
+    for func_arg in
+        select * from pgq.maint_tables_to_vacuum()
+    loop
+        return next;
+    end loop;
+
+    return;
+end;
+$$ language plpgsql;
+
