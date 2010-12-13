200fae95a06ffc0a64712a7cfd46a948b704294e
Marko Kreen <markokr@gmail.com>
2010-12-13 10:34:20 +0200

    bump db ver

--- sql/pgq/functions/pgq.version.sql
+++ sql/pgq/functions/pgq.version.sql
@@ -7,7 +7,7 @@ returns text as $$
 --      that is only bumped when PGQ database code changes.
 -- ----------------------------------------------------------------------
 begin
-    return '3.0.0.3';
+    return '3.0.0.4';
 end;
 $$ language plpgsql;
 
