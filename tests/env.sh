73f2a067ccbdd94e0ed25b48bc1586146032dce7
Marko Kreen <markokr@gmail.com>
2011-01-10 16:32:24 +0200

    tests: default to tcp connection

--- tests/env.sh
+++ tests/env.sh
@@ -14,3 +14,8 @@ PATH=$pfx/bin:$PATH
 #LD_LIBRARY_PATH=/opt/apps/py26/lib:$LD_LIBRARY_PATH
 #PATH=/opt/apps/py26/bin:$PATH
 export PYTHONPATH PATH LD_LIBRARY_PATH PATH
+
+PGHOST=localhost
+export PGHOST
+
+
