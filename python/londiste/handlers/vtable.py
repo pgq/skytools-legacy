d53ffb14a58bde2f632258f980225db2e7c00d22
Marko Kreen <markokr@gmail.com>
2011-09-21 16:23:04 +0300

    Rename fake_local to vtable, move to separate file

---
+++ python/londiste/handlers/vtable.py
@@ -0,0 +1,25 @@
+"""Virtual Table.
+"""
+
+from londiste.handler import BaseHandler
+
+__all__ = ['VirtualTableHandler', 'FakeLocalHandler']
+
+class VirtualTableHandler(BaseHandler):
+    """Virtual Table.
+
+    """
+    handler_name = 'vtable'
+
+    def add(self, trigger_arg_list):
+        trigger_arg_list.append('virtual_table')
+
+    def needs_table(self):
+        return False
+
+class FakeLocalHandler(VirtualTableHandler):
+    """Deprecated compat name for vtable."""
+    handler_name = 'fake_local'
+
+__londiste_handlers__ = [VirtualTableHandler, FakeLocalHandler]
+
