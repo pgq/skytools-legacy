ca66a63e624a4baabf4fb30179fafc54ecdbcdb1
Marko Kreen <markokr@gmail.com>
2012-01-10 22:40:55 +0100

    pgq.version(): 3.0.0.13

--- sql/pgq/functions/pgq.version.sql
+++ sql/pgq/functions/pgq.version.sql
@@ -7,7 +7,7 @@ returns text as $$
 --      that is only bumped when PGQ database code changes.
 -- ----------------------------------------------------------------------
 begin
-    return '3.0.0.12';
+    return '3.0.0.13';
 end;
 $$ language plpgsql;
 
