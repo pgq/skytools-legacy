128f094b0852944366856bdc56badb1b27dad40c
Marko Kreen <markokr@gmail.com>
2012-05-02 16:45:00 +0300

    Group-access roles for SQL functions (draft)
    This is attempt for fine-grained access rights for all
    Skytools SQL schemas.  As it still needs review,
    the rights are not activated by default, instead
    following sql files are generated:
    
      newgrants_<schema>.sql - applies new rights, drop old public access
    
      oldgrants_<schema>.sql - restores old rights - public execute
      		           privilege to all functions
    
    Only thing that is active by default is creation of new
    groups in upgrade functions.
    
    New access roles:
    
    pgq_reader
        Can consume queues (source-side)
    
    pgq_writer
        Can write into queues (source-side / dest-side)
        Can use pgq_node/pgq_ext schema as regular
        consumer (dest-side)
    
    pgq_admin
        Admin operations on queues, required for CascadedWorker on dest-side.
        Member of pgq_reader and pgq_writer.
    
    londiste_reader
        Member of pgq_reader, needs additional read access to tables.
        (source-side)
    
    londiste_writer
        Member of pgq_admin, needs additional write access to tables.
        (dest-side)

--- sql/pgq/structure/grants.sql
+++ sql/pgq/structure/grants.sql
@@ -1,5 +1,8 @@
 
+
 grant usage on schema pgq to public;
+
+-- old default grants
 grant select on table pgq.consumer to public;
 grant select on table pgq.queue to public;
 grant select on table pgq.tick to public;
@@ -7,3 +10,4 @@ grant select on table pgq.queue to public;
 grant select on table pgq.subscription to public;
 grant select on table pgq.event_template to public;
 grant select on table pgq.retry_queue to public;
+
---
+++ sql/pgq_node/structure/grants.sql
@@ -0,0 +1,3 @@
+
+grant usage on schema pgq_node to public;
+
