03ab113238fa11d9f1c451feb586f4928dbabad4
Marko Kreen <markokr@gmail.com>
2012-05-10 21:29:43 +0300

    Refresh debian packaging

---
+++ debian/genpg.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+
+# generate per-version files
+
+for v in 8.3 8.4 9.0 9.1 9.2; do
+
+echo "usr/share/doc/postgresql-$v" > "postgresql-$v-pgq3.dirs"
+
+cat > "postgresql-$v-pgq3.docs" <<EOF
+sql/pgq/README.pgq
+sql/pgq_ext/README.pgq_ext
+EOF
+
+cat > "postgresql-$v-pgq3.install" <<EOF
+usr/lib/postgresql/$v/lib/pgq_triggers.so
+usr/lib/postgresql/$v/lib/pgq_lowlevel.so
+usr/share/postgresql/$v/contrib/pgq.upgrade.sql
+usr/share/postgresql/$v/contrib/pgq_triggers.sql
+usr/share/postgresql/$v/contrib/pgq_lowlevel.sql
+usr/share/postgresql/$v/contrib/pgq_node.sql
+usr/share/postgresql/$v/contrib/pgq_coop.upgrade.sql
+usr/share/postgresql/$v/contrib/pgq_ext.sql
+usr/share/postgresql/$v/contrib/londiste.sql
+usr/share/postgresql/$v/contrib/pgq_node.upgrade.sql
+usr/share/postgresql/$v/contrib/pgq.sql
+usr/share/postgresql/$v/contrib/pgq_coop.sql
+usr/share/postgresql/$v/contrib/londiste.upgrade.sql
+usr/share/postgresql/$v/contrib/uninstall_pgq.sql
+usr/share/postgresql/$v/contrib/newgrants_londiste.sql
+usr/share/postgresql/$v/contrib/newgrants_pgq_coop.sql
+usr/share/postgresql/$v/contrib/newgrants_pgq_ext.sql
+usr/share/postgresql/$v/contrib/newgrants_pgq_node.sql
+usr/share/postgresql/$v/contrib/newgrants_pgq.sql
+usr/share/postgresql/$v/contrib/oldgrants_londiste.sql
+usr/share/postgresql/$v/contrib/oldgrants_pgq_coop.sql
+usr/share/postgresql/$v/contrib/oldgrants_pgq_ext.sql
+usr/share/postgresql/$v/contrib/oldgrants_pgq_node.sql
+usr/share/postgresql/$v/contrib/oldgrants_pgq.sql
+EOF
+
+done
