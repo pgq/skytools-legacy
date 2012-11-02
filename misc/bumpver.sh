fc77ccf7fff52844bd6a36a9bce3b22ec2b1bd35
Marko Kreen <markokr@gmail.com>
2012-11-02 10:28:11 +0200

    bumpver: just check if bump is needed

--- misc/bumpver.sh
+++ misc/bumpver.sh
@@ -5,8 +5,10 @@ for s in pgq pgq_node pgq_coop londiste pgq_ext; do
   fn="sql/$s/functions/$s.version.sql"
   fver=$(git log --raw -n 1 "$fn" | head -1)
   test "$sver" = "$fver" && continue
-  nver=`./misc/bumpver.py "$fn"`
-  git commit -m "$s.version(): $nver" "$fn"
+
+  echo "$s needs new version"
+  #nver=`./misc/bumpver.py "$fn"`
+  #git commit -m "$s.version(): $nver" "$fn"
 done
 
 grep 'return ' sql/*/functions/*.version.sql
