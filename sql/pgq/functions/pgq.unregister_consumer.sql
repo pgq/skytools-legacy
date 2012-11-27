024bf5de509c2d0db2cc5abbce2a5387758bec4d
Petr Jelinek <git@pjmodos.net>
2012-11-27 21:16:33 +0100

    pgq.unregister_consumer: don't unregister all subconsumers in subscription when subconsumer was given on input

--- sql/pgq/functions/pgq.unregister_consumer.sql
+++ sql/pgq/functions/pgq.unregister_consumer.sql
@@ -24,14 +24,19 @@ returns integer as $$
 declare
     x_sub_id integer;
     _sub_id_cnt integer;
+    _consumer_id integer;
+    _is_subconsumer boolean;
 begin
-    select s.sub_id into x_sub_id
-        from pgq.subscription s, pgq.consumer c, pgq.queue q
-        where s.sub_queue = q.queue_id
-          and s.sub_consumer = c.co_id
-          and q.queue_name = x_queue_name
-          and c.co_name = x_consumer_name
-        for update of s;
+    select s.sub_id, c.co_id,
+           -- subconsumers can only have both null or both not null - main consumer for subconsumers has only one not null
+           (s.sub_last_tick IS NULL AND s.sub_next_tick IS NULL) OR (s.sub_last_tick IS NOT NULL AND s.sub_next_tick IS NOT NULL)
+      into x_sub_id, _consumer_id, _is_subconsumer
+      from pgq.subscription s, pgq.consumer c, pgq.queue q
+     where s.sub_queue = q.queue_id
+       and s.sub_consumer = c.co_id
+       and q.queue_name = x_queue_name
+       and c.co_name = x_consumer_name
+       for update of s;
     if not found then
         return 0;
     end if;
@@ -41,15 +46,27 @@ begin
         from pgq.subscription
        where sub_id = x_sub_id;
 
-    -- retry events
-    delete from pgq.retry_queue
-        where ev_owner = x_sub_id;
+    -- delete only one subconsumer
+    if _sub_id_cnt > 1 and _is_subconsumer then
+        delete from pgq.subscription
+              where sub_id = x_sub_id
+                and sub_consumer = _consumer_id;
 
-    -- this will drop subconsumers too
-    delete from pgq.subscription
-        where sub_id = x_sub_id;
+        return 1;
+    else
+        -- delete main consumer (including possible subconsumers)
 
-    return _sub_id_cnt;
+        -- retry events
+        delete from pgq.retry_queue
+            where ev_owner = x_sub_id;
+
+        -- this will drop subconsumers too
+        delete from pgq.subscription
+            where sub_id = x_sub_id;
+    
+        return _sub_id_cnt;
+    end if;
+
 end;
 $$ language plpgsql security definer;
 
