b2f746fac824c4d71fdf13472f967fcfec6aff54
Marko Kreen <markokr@gmail.com>
2012-11-29 14:28:41 +0200

    Bump pgq schema ver to 3.1.3

--- sql/pgq/functions/pgq.version.sql
+++ sql/pgq/functions/pgq.version.sql
@@ -7,7 +7,7 @@ returns text as $$
 --      and only bumped when database code changes.
 -- ----------------------------------------------------------------------
 begin
-    return '3.1.2';
+    return '3.1.3';
 end;
 $$ language plpgsql;
 
