866c04665a6de62240df7d212e2f45a4cfaf20b3
martinko <gamato@users.sf.net>
2013-02-21 13:36:01 +0100

    londiste.drop_obsolete_partitions: reformatted

--- sql/londiste/functions/londiste.drop_obsolete_partitions.sql
+++ sql/londiste/functions/londiste.drop_obsolete_partitions.sql
@@ -1,11 +1,11 @@
 
 create or replace function londiste.drop_obsolete_partitions
 (
-  in i_parent_table text,
-  in i_retention_period interval,
-  in i_partition_period text
+    in i_parent_table text,
+    in i_retention_period interval,
+    in i_partition_period text
 )
-  returns void
+    returns void
 as $$
 -------------------------------------------------------------------------------
 --  Function: londiste.drop_obsolete_partitions(3)
@@ -18,44 +18,42 @@ as $$
 --    i_partition_period    One of: year, month, day, hour
 -------------------------------------------------------------------------------
 declare
-  _schema text not null := lower (split_part (i_parent_table, '.', 1));
-  _table  text not null := lower (split_part (i_parent_table, '.', 2));
-  _part   text;
-  _expr   text;
-  _dfmt   text;
+    _schema text not null := lower (split_part (i_parent_table, '.', 1));
+    _table  text not null := lower (split_part (i_parent_table, '.', 2));
+    _part   text;
+    _expr   text;
+    _dfmt   text;
 begin
-  if i_partition_period in ('year', 'yearly') then
-    _expr := '_[0-9]{4}';
-    _dfmt := '_YYYY';
-  elsif i_partition_period in ('month', 'monthly') then
-    _expr := '_[0-9]{4}_[0-9]{2}';
-    _dfmt := '_YYYY_MM';
-  elsif i_partition_period in ('day', 'daily') then
-    _expr := '_[0-9]{4}_[0-9]{2}_[0-9]{2}';
-    _dfmt := '_YYYY_MM_DD';
-  elsif i_partition_period in ('hour', 'hourly') then
-    _expr := '_[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}';
-    _dfmt := '_YYYY_MM_DD_HH24';
-  else
-    raise exception 'not supported i_partition_period: %', i_partition_period;
-  end if;
+    if i_partition_period in ('year', 'yearly') then
+        _expr := '_[0-9]{4}';
+        _dfmt := '_YYYY';
+    elsif i_partition_period in ('month', 'monthly') then
+        _expr := '_[0-9]{4}_[0-9]{2}';
+        _dfmt := '_YYYY_MM';
+    elsif i_partition_period in ('day', 'daily') then
+        _expr := '_[0-9]{4}_[0-9]{2}_[0-9]{2}';
+        _dfmt := '_YYYY_MM_DD';
+    elsif i_partition_period in ('hour', 'hourly') then
+        _expr := '_[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}';
+        _dfmt := '_YYYY_MM_DD_HH24';
+    else
+        raise exception 'not supported i_partition_period: %', i_partition_period;
+    end if;
 
-  if length (_table) = 0 then
-    _table := _schema;
-    _schema := 'public';
-  end if;
+    if length (_table) = 0 then
+        _table := _schema;
+        _schema := 'public';
+    end if;
 
-  for _part in
-    select quote_ident (t.schemaname) ||'.'|| quote_ident (t.tablename)
-      from pg_catalog.pg_tables t
-     where t.schemaname = _schema
-       and t.tablename ~ ('^'|| _table || _expr ||'$')
-       and t.tablename < _table || to_char (now() - i_retention_period, _dfmt)
-  loop
-    raise notice 'DROP TABLE %', _part;
-    execute 'drop table '|| _part;
-  end loop;
+    for _part in
+        select quote_ident (t.schemaname) ||'.'|| quote_ident (t.tablename)
+          from pg_catalog.pg_tables t
+         where t.schemaname = _schema
+           and t.tablename ~ ('^'|| _table || _expr ||'$')
+           and t.tablename < _table || to_char (now() - i_retention_period, _dfmt)
+    loop
+        raise notice 'DROP TABLE %', _part;
+        execute 'drop table '|| _part;
+    end loop;
 end;
 $$ language plpgsql;
-
--- vim:et:sw=2:ts=2:nowrap:
